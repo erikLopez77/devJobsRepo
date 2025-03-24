1a4d1429835f851b417f6273080577c64efec4d6
ErikLopez <espinozalopezerik@gmail.com>
2025-03-23 22:23:17 -0600

    layout, primer vista hdbs

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const router = require('./routes/index')();
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('funciona')
-});
+//habilitar handlebars como view
+app.engine('handlebars', engine({
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'handlebars');
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', router);
 
 app.listen(5000);
