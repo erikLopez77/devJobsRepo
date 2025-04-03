dafff473149652aae290eaf92907d79aec27c7cc
ErikLopez <espinozalopezerik@gmail.com>
2025-04-02 22:28:32 -0600

    dependencias, variables de entorno

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const router = require('./routes/index')();
 
+require('dotenv').config({ path: 'variables.env' })
+
 const app = express();
 
 //habilitar handlebars como view
@@ -16,4 +18,4 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', router);
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
