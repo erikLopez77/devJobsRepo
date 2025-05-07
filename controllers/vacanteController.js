efb547124646568c3e9db9319cc423e69683516c
ErikLopez <espinozalopezerik@gmail.com>
2025-05-06 21:08:20 -0600

    intento de input

--- controllers/vacanteController.js
+++ controllers/vacanteController.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Vacante = mongoose.model('Vacante');
 const { check, validationResult } = require('express-validator');
 
+const multer = require('multer');
+const shortId = require('shortid');
 
 exports.formularioNuevaVacante = (req, res) => {
     res.render('nueva-vacante', {
@@ -111,3 +113,45 @@ const verificarAutor = (vacante = {}, usuario = {}) => {
     }
     return true;
 }
+//subir archivos en PDF
+exports.subirCV = (req, res, next) => {
+    upload(req, res, function (error) {
+        if (error) {
+            if (error instanceof multer.MulterError) {
+                if (error.code === 'LIMIT_FILE_SIZE') {
+                    req.flash('error', 'El archivo es muy grande, máximo 100Kb');
+                } else {
+                    req.flash('error', error.message);
+                }
+            } else {
+                req.flash('error', error.message);
+            }
+            res.redirect('back');
+            return;
+        } else {
+            return next();
+        }
+    });
+}
+
+//opciones de multer
+const configuracionMulter = {
+    limits: { fileSize: 100000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname + '../../public/uploads/cv');
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`)
+        }
+    }), fileFilter(req, file, cb) {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+            // el cb se ejecuta como true o false, true si se acepta img
+            cb(null, true);
+        } else {
+            cb(new Error('Formato no válido'), false);
+        }
+    }
+}
+const upload = multer(configuracionMulter).single('cv');
